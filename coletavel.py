69dca62bbb7893882c2dda98371c063385859f84
Vitor Negromonte <vitor.negromonte@ufpe.br>
2023-09-20 16:53:36 -0300

    feat(feature/coletaveis): mudanças nos métodos

--- coletavel.py
+++ coletavel.py
@@ -1,23 +1,18 @@
-import pygame
 import pygame as pg
 import random
 
 pg.init()
-
 largura_tela = 400
 altura_tela = 600
 tela = pg.display.set_mode((largura_tela, altura_tela))
 # start_ticks = pg.time.get_ticks()  # starter tick
 
-valor_inicial = 1
 meta = random.randint(0,100)
+valor_inicial = 1
 branco = (255, 255, 255)
 fonte = pg.font.Font('assets/fonts/pixel-art.ttf', 20)  # Fonte para exibir o contador
-
 # Lista de operações matemáticas
-operacoes = ["+", "-", "x", "/"]  # Use "x" para representar multiplicação
-
-
+operacoes = ["+", "-", "x", "/"]  #"x" para representar multiplicação
 
 class Coletavel:
     def __init__(self):
@@ -26,33 +21,8 @@ class Coletavel:
         self.velocidade = 0.1  # Defina uma velocidade constante
         self.operacao = random.choice(operacoes)  # Escolha aleatória de operação
         self.operando = random.randint(1, 10)  # Número aleatório pro operando
-        
         self.resposta = self.calcular_resposta()
 
-    def calcular_opercao(self):
-        operacao = random.choice(operacoes)  # Escolha aleatória de operação
-        operando = random.randint(1, 10)  # Número aleatório pro operando
-        resultado = valor_inicial
-        while True:
-            
-            if operacao == "+":
-                resultado += operando
-            elif operacao == "-":
-                resultado -= operando
-            elif operacao == "x":
-                resultado *= operando
-            elif operacao == "/":
-                resultado /= operando
-
-            if resultado == meta:
-                break
-            else:
-                # Se o resultado não atingir a meta, escolha outra operação aleatória
-                operacao = random.choice(operacoes)
-                operando = random.randint(1, 10)
-
-        return operacao, operando
-    
     def calcular_resposta(self):
         resposta = valor_inicial  # Inicialize com o valor_inicial
         if self.operacao == "+":
@@ -81,11 +51,6 @@ pontuacao = 0
 intervalo_geracao = 2000  # Defina o intervalo de geração (menos objetos ao mesmo tempo)
 limite_geracao = intervalo_geracao 
 
-# # Posição inicial do jogador
-# jogador_x = largura_tela // 2 - 20
-# jogador_y = altura_tela - 40
-# jogador_velocidade = 5  # Velocidade de movimento do jogador
-
 # Jogador
 jogador_largura = 50
 jogador_altura = 20
@@ -109,42 +74,22 @@ while executando:
         contador = 0
 
         limite_geracao = random.randint(1, intervalo_geracao)
-
-    for objeto in objetos_caindo:
-        objeto.mover()
-
+        
     tela.fill((0, 0, 0))
 
     for objeto in objetos_caindo:
+        objeto.mover()
         objeto.desenhar_objeto()
-
-    # jogador_rect = pg.Rect(largura_tela // 2 - 20, altura_tela - 40, 40, 40)  # Posição e tamanho do jogador
-    # jogador = pg.draw.rect(tela, branco, jogador_rect)
 
     jogador_rect = pg.Rect(jogador_x, jogador_y, 40, 40)  # Posição e tamanho do jogador
     jogador = pg.draw.rect(tela, branco, jogador_rect)
 
-    # jogador_largura = 50
-    # jogador_altura = 20
-    # jogador_x = (largura_tela - jogador_largura) // 2
-    # jogador_y = altura_tela - jogador_altura
-    # velocidade_jogador = 5
-
     # Capturar entrada do teclado
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and jogador_x > 0:
+    keys = pg.key.get_pressed()
+    if keys[pg.K_LEFT] and jogador_x > 0:
         jogador_x -= velocidade_jogador
-    if keys[pygame.K_RIGHT] and jogador_x < largura_tela - jogador_largura:
+    if keys[pg.K_RIGHT] and jogador_x < largura_tela - jogador_largura:
         jogador_x += velocidade_jogador
-
-    # #Lógica de movimento do jogador
-    # teclas = pg.key.get_pressed()
-    # if teclas[pg.K_LEFT]:
-    #     jogador_x -= jogador_velocidade
-    # if teclas[pg.K_RIGHT]:
-    #     jogador_x += jogador_velocidade
-
-
         
     # Verifique colisões
     for objeto in objetos_caindo:
@@ -159,11 +104,11 @@ while executando:
     tela.blit(texto_meta, (250, 10))
     
     # Exiba o valor inicial na tela
-    valor_surface = fonte.render(f"Valor: {valor_inicial}", True, branco)
+    valor_surface = fonte.render(f"Valor: {valor_inicial:.2f}", True, branco)
     tela.blit(valor_surface, (10, 10))
 
     # Verifique se o jogador atingiu a meta
-    if valor_inicial >= meta:
+    if valor_inicial == meta:
         executando = False
 
     pg.display.flip()
